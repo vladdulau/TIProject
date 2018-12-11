79a2aa17e8838c5d8c810ff2ffa4eec8d9f5d8af
Vlad Dulau <vlad.dulau@outlook.com>
2018-12-11 20:15:59 +0200

    Add randomization for photos

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -12,7 +12,7 @@ public partial class _Default : Page
         if (!IsPostBack)
         {
             Label1.Text = DateTime.Now.ToString();
-            Image1.ImageUrl = "~/Images/payment.jpg";
+            Image1.ImageUrl = "~/Images/1.jpg";
         }
        
         
@@ -20,15 +20,24 @@ public partial class _Default : Page
 
     protected void Timer2_Tick(object sender, EventArgs e)
     {
+        var imageUrl= Image1.ImageUrl.ToString();
         Label1.Text = DateTime.Now.ToString();
 
-        if (Image1.ImageUrl == "~/Images/payment.jpg")
-        {
-            Image1.ImageUrl = "~/Images/salary.jpg";
-        }
-        else
-        {
-            Image1.ImageUrl = "~/Images/payment.jpg";
-        }
+        if (imageUrl == "~/Images/1.jpg")
+            imageUrl = "~/Images/2.jpg";
+
+        if (imageUrl == "~/Images/2.jpg")
+            imageUrl = "~/Images/3.jpg";
+
+        if (imageUrl == "~/Images/3.jpg")
+            imageUrl = "~/Images/4.jpg";
+
+        if (imageUrl == "~/Images/4.jpg")
+            imageUrl = "~Images/1.jpg";
+
+        
+        Random random = new Random();
+        int i = random.Next(1, 4);
+        Image1.ImageUrl = "~/Images/" + i.ToString() + ".jpg";
     }
 }
