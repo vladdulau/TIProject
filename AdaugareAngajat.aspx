9cc7ccd6f80db2f8a2b1b73472feb6bed97bb3eb
Vlad <Vlad@DESKTOP-4FDD4DJ>
2018-12-26 19:47:22 +0200

    Add 'AdaugareAngajat' feature

---
+++ AdaugareAngajat.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="AdaugareAngajat.aspx.cs" Inherits="AdaugareAngajat" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="Server">
+    <div class="search-view">
+        <div class="row">
+            <asp:TextBox ID="textCauta" runat="server" BorderStyle="Inset"></asp:TextBox>
+            <asp:Button ID="buttonCauta" runat="server" Text="Cauta" BorderStyle="Outset" OnClick="buttonCauta_Click" />
+        </div>
+    </div>
+    <div class="tabel-salarii">
+        <asp:GridView ID="gridSalarii" AutoGenerateColumns="True" AllowPaging="True" BorderStyle="Inset" PageSize="6" runat="server" Width="700px" DataKeyNames="NR_CRT" OnPageIndexChanging="gridSalarii_PageIndexChanging">
+            <Columns>
+                <asp:BoundField DataField="NR_CRT" HeaderText="Nr. Crt" SortExpression="NR_CRT" />
+                <asp:BoundField DataField="NUME" HeaderText="Nume" SortExpression="NUME" />
+                <asp:BoundField DataField="PRENUME" HeaderText="Prenume" SortExpression="PRENUME" />
+                <asp:BoundField DataField="FUNCTIE" HeaderText="Functie" SortExpression="FUNCTIE" />
+            </Columns>
+            <HeaderStyle BackColor="DarkGray" ForeColor="White" HorizontalAlign="Center" VerticalAlign="Middle" />
+            <RowStyle BackColor="White" />
+        </asp:GridView>
+    </div>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <div class="actualizare-campuri">
+        <div class="aranjare-campuri">
+            <div class="row">
+                <asp:Label ID="numeLabel" runat="server" Text="Nume "></asp:Label>
+                <asp:TextBox ID="NUME" runat="server" BorderStyle="Ridge" Width="200px"></asp:TextBox>
+                <asp:Label ID="prenumeLabel" runat="server" Text="Prenume"></asp:Label>
+                <asp:TextBox ID="PRENUME" runat="server" BorderStyle="Ridge" Width="200px"></asp:TextBox>
+                <asp:Label ID="functieLabel" runat="server" Text="Functie"></asp:Label>
+                <asp:TextBox ID="FUNCTIE" runat="server" BorderStyle="Ridge"  Width="200px"></asp:TextBox>
+            </div>
+        </div>
+        <p></p>
+         <p></p>
+        <div class="row">
+            <asp:Label ID="salarBazaLabel" runat="server" Text="Salar baza"></asp:Label>
+            <asp:TextBox ID="SALAR_BAZA" runat="server" BorderStyle="Ridge" Enabled="true" Width="100px"></asp:TextBox>
+            <asp:Label ID="sporLabel" runat="server" Text="Spor "></asp:Label>
+            <asp:TextBox ID="SPOR" runat="server" BorderStyle="Ridge" Enabled="true" Width="100px"></asp:TextBox>
+            <asp:Label ID="premiiBruteLabel" runat="server" Text="Premii brute"></asp:Label>
+            <asp:TextBox ID="PREMII_BRUTE" runat="server" BorderStyle="Ridge" Enabled="true" Width="100px"></asp:TextBox>
+        </div>
+        <p></p>
+    <p></p>
+        <p></p>
+    <p></p>
+        <div class="row">
+            <asp:Label ID="retineriLabel" runat="server" Text="Retineri"></asp:Label>
+            <asp:TextBox ID="RETINERI" runat="server" BorderStyle="Ridge" Enabled="true" Width="100px"></asp:TextBox>
+           
+        </div>
+        <p></p>
+    <p></p>
+        <div class="search-view">
+            <div class="row">
+                <asp:Button ID="btnAdauga" runat="server" Text="Adauga" BorderStyle="Outset" OnClick="buttonAdauga_Click" />
+                <asp:Button ID="btnRenunta" runat="server" Text="Renunta" BorderStyle="Outset" OnClick="buttonRenunta_Click" />
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
+
---
+++ AdaugareAngajat.aspx.cs
@@ -0,0 +1,208 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AdaugareAngajat : System.Web.UI.Page
+{
+    OracleConnection conn;
+    OracleCommand command;
+    OracleDataAdapter dataAdapter;
+    DataSet dataSet;
+
+    string query = "SELECT NR_CRT, NUME, PRENUME, FUNCTIE FROM ANGAJATI ORDER BY NR_CRT";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            try
+            {
+                if (conn == null || conn.State.Equals(ConnectionState.Closed))
+                {
+                    conn = new OracleConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+                    conn.Open();
+                    command = new OracleCommand(query, conn);
+                    dataAdapter = new OracleDataAdapter(command);
+                    dataSet = new DataSet();
+
+                    dataAdapter.Fill(dataSet, "Angajati");
+                    this.gridSalarii.DataSource = dataSet.Tables["Angajati"].DefaultView;
+                    gridSalarii.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+
+    protected void buttonCauta_Click(object sender, EventArgs e)
+    {
+
+        var searchText = this.textCauta.Text;
+
+        query = "SELECT NR_CRT, NUME, PRENUME, FUNCTIE FROM ANGAJATI WHERE LOWER(NUME) LIKE LOWER(\'%" + searchText + "%\') OR LOWER(PRENUME) LIKE LOWER(\'%" + searchText + "%\') OR LOWER(FUNCTIE) LIKE LOWER(\'%" + searchText + "%\')";
+
+        try
+        {
+            if (conn == null || conn.State.Equals(ConnectionState.Closed))
+            {
+                conn = new OracleConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+                conn.Open();
+                command = new OracleCommand(query, conn);
+                dataAdapter = new OracleDataAdapter(command);
+                dataSet = new DataSet();
+
+                dataAdapter.Fill(dataSet, "Angajati");
+                this.gridSalarii.DataSource = dataSet.Tables["Angajati"].DefaultView;
+                gridSalarii.DataBind();
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+
+    public void cleanAllFields()
+    {
+        this.textCauta.Text = "";
+        this.NUME.Text = "";
+        this.PRENUME.Text = "";
+        this.FUNCTIE.Text = "";
+        this.SALAR_BAZA.Text = "";
+        this.SPOR.Text = "";
+        this.PREMII_BRUTE.Text = "";
+        this.RETINERI.Text = "";
+    }
+
+    protected void buttonRenunta_Click(object sender, EventArgs e)
+    {
+        cleanAllFields();
+
+        query = "SELECT NR_CRT, NUME, PRENUME, FUNCTIE FROM ANGAJATI ORDER BY NR_CRT";
+
+        try
+        {
+            if (conn == null || conn.State.Equals(ConnectionState.Closed))
+            {
+                conn = new OracleConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+                conn.Open();
+                command = new OracleCommand(query, conn);
+                dataAdapter = new OracleDataAdapter(command);
+                dataSet = new DataSet();
+
+                dataAdapter.Fill(dataSet, "Angajati");
+                this.gridSalarii.DataSource = dataSet.Tables["Angajati"].DefaultView;
+                gridSalarii.DataBind();
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+
+    protected void gridSalarii_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+
+        try
+        {
+            if (conn == null || conn.State.Equals(ConnectionState.Closed))
+            {
+                conn = new OracleConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+                conn.Open();
+                command = new OracleCommand(query, conn);
+                dataAdapter = new OracleDataAdapter(command);
+                dataSet = new DataSet();
+
+                dataAdapter.Fill(dataSet, "Angajati");
+                this.gridSalarii.DataSource = dataSet.Tables["Angajati"].DefaultView;
+                this.gridSalarii.PageIndex = e.NewPageIndex;
+                gridSalarii.DataBind();
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+
+
+
+    
+
+    protected void buttonAdauga_Click(object sender, EventArgs e)
+    {
+        if (this.NUME.Text == null || this.NUME.Text == "" || this.PRENUME.Text == null || this.PRENUME.Text == "" || this.FUNCTIE.Text == null || this.FUNCTIE.Text == ""
+            || this.SALAR_BAZA.Text == null || this.SALAR_BAZA.Text == "")
+        {
+            ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('Completati toate campurile pentru a adauga un angajat');", true);
+        }
+        else
+        {
+            string nume = this.NUME.Text;
+            string prenume = this.PRENUME.Text;
+            string functie = this.FUNCTIE.Text;
+            decimal salar_baza = Decimal.Parse(this.SALAR_BAZA.Text);
+            decimal spor = this.SPOR.Text == null || this.SPOR.Text == "" ? 0 : Decimal.Parse(this.SPOR.Text);
+            decimal premii_brute = this.PREMII_BRUTE.Text == null || this.PREMII_BRUTE.Text == "" ? 0 : Decimal.Parse(this.PREMII_BRUTE.Text);
+            decimal retineri = this.RETINERI.Text == null || this.RETINERI.Text == "" ? 0 : Decimal.Parse(this.RETINERI.Text);
+
+            try
+            {
+                if (conn == null || conn.State.Equals(ConnectionState.Closed))
+                {
+                    conn = new OracleConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+                    conn.Open();
+                    query = "INSERT INTO ANGAJATI (NUME, PRENUME, FUNCTIE, SALAR_BAZA, SPOR, PREMII_BRUTE, RETINERI) VALUES( \'" + nume + "\',\'" + prenume + "\',\'" + functie + "\'," + salar_baza + ", " + spor + ", " + premii_brute + ", " + retineri + ")";
+
+                    command = new OracleCommand(query, conn);
+                }
+
+                command.ExecuteNonQuery();
+
+                query = "SELECT NR_CRT, NUME, PRENUME, FUNCTIE FROM ANGAJATI ORDER BY NR_CRT";
+                command = new OracleCommand(query, conn);
+                dataAdapter = new OracleDataAdapter(command);
+                dataSet = new DataSet();
+
+                dataAdapter.Fill(dataSet, "Angajati");
+                this.gridSalarii.DataSource = dataSet.Tables["Angajati"].DefaultView;
+                gridSalarii.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            cleanAllFields();
+        }
+    }
+}
